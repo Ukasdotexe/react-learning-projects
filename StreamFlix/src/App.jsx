d110098d50ea3daa340d6f211f8a440067f9a2a1
Ukas <siqxdpo@gmail.com>
2025-02-16 22:55:56 +0100

    Create Movie component to display movie poster, title, and year; add styling for movie items with hover effect and grid layout

--- StreamFlix/src/App.jsx
+++ StreamFlix/src/App.jsx
@@ -1,5 +1,52 @@
 import { useState } from "react";
 
+const tempMovieData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0133093",
+    Title: "The Matrix",
+    Year: "1999",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt6751668",
+    Title: "Parasite",
+    Year: "2019",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+  },
+];
+
+const tempWatchedData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    runtime: 148,
+    imdbRating: 8.8,
+    userRating: 10,
+  },
+  {
+    imdbID: "tt0088763",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    runtime: 116,
+    imdbRating: 8.5,
+    userRating: 9,
+  },
+];
+
 function App() {
   return (
     <>
@@ -25,11 +72,28 @@ function Header() {
 }
 
 function MoviesList() {
-  return <ul className="movies-list">Movies list</ul>;
+  return (
+    <ul className="movies-list">
+      {tempMovieData.map((movie) => {
+        return <Movie key={movie.imdbID} movie={movie} />;
+      })}
+    </ul>
+  );
 }
 
 function MovieOverView() {
   return <div className="movie-overview">Movie OverView</div>;
 }
 
+function Movie({ movie }) {
+  return (
+    <div className="movie">
+      <img src={movie.Poster} />
+      <div className="movie__info">
+        <span className="movie__name">{movie.Title}</span>
+        <span className="movie__year">📅 {movie.Year}</span>
+      </div>
+    </div>
+  );
+}
 export default App;
