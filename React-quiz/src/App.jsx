b320ee67cc892cd587c41331311d3de05be5c394
Ukas <siqxdpo@gmail.com>
2025-02-26 05:59:47 +1100

    feat : add Main component

--- React-quiz/src/App.jsx
+++ React-quiz/src/App.jsx
@@ -3,6 +3,7 @@ function App() {
   return (
     <div className="app">
       <Header />
+      <Main />
     </div>
   );
 }
@@ -16,7 +17,29 @@ function Header() {
   );
 }
 
+function Main() {
+  return (
+    <main className="main">
+      <div className="start">
+        <h2>Welcome to The React Quiz</h2>
+        <h3>X questions to test your React mastery</h3>
+        <button className="btn">Let's Start</button>
+      </div>
+      <div className="progress">
+        <progress value="0" max="280"></progress>
+        <span>Question 5/12</span>
+        <span>0/ 280 points</span>
+      </div>
+      <div className="options">
+        <button className="btn btn-option">Angular</button>
+        <button className="btn btn-option">React</button>
+        <button className="btn btn-option">Vue</button>
+        <button className="btn btn-option">Angular</button>
+      </div>
+      <div style={{ textAlign: "left" }}>
+        <button className="btn ">07:50</button>
+      </div>
+    </main>
+  );
+}
 export default App;
-// feat: add header section
-// - Create a new component for the header  page
-// - Add styling for the component and other future components
