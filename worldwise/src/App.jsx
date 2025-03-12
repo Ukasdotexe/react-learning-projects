6d2070582293be1fca90352f7c7b1caebfd68598
Ukas <siqxdpo@gmail.com>
2025-03-13 05:36:33 +1100

    Added React Router and updated App structure

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,8 +1,29 @@
 //
 //
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+
 function App() {
-  return <div>World Wise </div>;
+  return (
+    <>
+      {/* <PageNav /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="pricing" element={<Pricing />}></Route>
+          <Route path="product" element={<Product />}></Route>
+          <Route path="app" element={<AppLayout />}></Route>
+          <Route path="*" element={<PageNotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
